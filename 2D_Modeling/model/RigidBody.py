fixed jacobian calc
had to invert the X_dot - X_dote

2D_Modeling/model/RigidBody.py
@@ -81,8 +81,8 @@ class RigidBody:
                                            self.center_of_buoyancy[1] * np.sin(theta))
         
         #Tow Force Moment
-        tow_force_moment = self.tow_force.magnitude * (np.sin(self.tow_force.delta_t - theta) * self.tow_force.location[0] +\
-                                                       np.cos(self.tow_force.delta_t - theta) * self.tow_force.location[1])
+        tow_force_moment = self.tow_force.magnitude * (np.sin(self.tow_force.delta_t - theta) * self.tow_force.location[0] \
+                                                       + np.cos(self.tow_force.delta_t - theta) * self.tow_force.location[1])
 
         #Control Force Moment
         control_force_moment = np.sum([(f.location[0] * (f.magnitude[0] * np.sin(f.alpha_i) + f.magnitude[1] * np.cos(f.alpha_i)) \

2D_Modeling/model/Simulation.py
@@ -307,8 +307,8 @@ class Simulation():
         body_acc_vector = np.hstack((bf_velocities, bf_accelerations))
         inertial_vector = T_full @ body_acc_vector
         inertial_acceleration = [inertial_vector[2],inertial_vector[3]]  # [x_ddot, z_ddot]         # Extract inertial-frame accelerations from the transformed vector
-        inertial_velocity_xxx = [inertial_vector[0],inertial_vector[1]]
-        inertial_velocity = [x_dot + inertial_acceleration[0] * self.sim.dt, z_dot + inertial_acceleration[1] * self.sim.dt] #as given by solver
+        inertial_velocity = [inertial_vector[0],inertial_vector[1]]
+        # inertial_velocity = [x_dot + inertial_acceleration[0] * self.sim.dt, z_dot + inertial_acceleration[1] * self.sim.dt] #as given by solver
         inertial_position = [x + inertial_velocity[0] * self.sim.dt, z + inertial_velocity[1] * self.sim.dt]           #as given by solver
         
 
@@ -362,11 +362,10 @@ class Simulation():
             # Simulate the system with the perturbed state
             xdot_ie = np.array(self.system_dynamics(0, perturbed_state))
             # Calculate the Jacobian element
-            jacobian[:, i] = (X_dot - xdot_ie) / epsilon
-            #print('pstate '+ str(perturbed_state))
-            #print('xdot pertu '+ str(xdot_ie))
+            jacobian[:, i] = (xdot_ie - X_dot) / epsilon
 
-        return jacobian, X
+        eigenvalues, eigenvectors = np.linalg.eig(jacobian)
+        return jacobian, eigenvalues, eigenvectors, X
 
     ##################################################################################################
     ######################################## EQUILIBRIUM STATE ########################################